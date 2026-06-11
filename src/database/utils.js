f9918398467d381d5c90cf273ce13a25eba32b2e
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-11 11:05:41 +0200

    Fix utils

--- src/database/utils.js
+++ src/database/utils.js
@@ -1,8 +1,8 @@
 const buildMongoUri = () => {
   const user = process.env.DB_USER;
   const pass = process.env.DB_PASSWORD;
-  const host = process.env.DB_HOST_CLI ?? 'localhost';
-  const port = process.env.DB_PORT_CLI ?? '27017';
+  const host = process.env.DB_HOST ?? 'localhost';
+  const port = process.env.DB_PORT ?? '27017';
   const db = process.env.DB_NAME ?? 'entropy_notifs_db';
 
   return `mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=admin`;
