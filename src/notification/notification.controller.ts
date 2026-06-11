4d1e2e83a079e485fbf24cc3f48db948dda208c4
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-11 16:11:30 +0200

    Add logging

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { NotificationService } from './notification.service';
 import {
@@ -10,15 +10,18 @@ import {
 
 @Controller('notification')
 export class NotificationController {
+  private readonly logger = new Logger(NotificationController.name);
   constructor(private readonly notificationService: NotificationService) {}
 
   @MessagePattern({ cmd: 'create_notification' })
   async createNotification(@Payload() data: CreateNotificationDto) {
+    this.logger.log('Received create notification record');
     return this.notificationService.create(data);
   }
 
   @MessagePattern({ cmd: 'find_all_notifications' })
   async findAllNotifications(@Payload() data: PaginationQueryDto) {
+    this.logger.log('Received find all notifications request');
     return this.notificationService.findAll(data);
   }
 
@@ -26,11 +29,13 @@ export class NotificationController {
   async findAllNotificationsForUser(
     @Payload() searchPagination: SearchPaginationQueryDto,
   ) {
+    this.logger.log('Received find all notifications for user request');
     return this.notificationService.findAllForUser(searchPagination);
   }
 
   @MessagePattern({ cmd: 'find_one_notification' })
   async findOneNotification(@Payload() id: string) {
+    this.logger.log('Received find one notification request for id: ' + id);
     return this.notificationService.findOne(id);
   }
 
@@ -38,11 +43,15 @@ export class NotificationController {
   async updateNotification(
     @Payload() payload: { id: string; updateData: UpdateNotificationDto },
   ) {
+    this.logger.log(
+      'Received update notification request for id: ' + payload.id,
+    );
     return this.notificationService.update(payload.id, payload.updateData);
   }
 
   @MessagePattern({ cmd: 'remove_notification' })
   async removeNotification(@Payload() id: string) {
+    this.logger.log('Received remove notification request for id: ' + id);
     return this.notificationService.remove(id);
   }
 }
